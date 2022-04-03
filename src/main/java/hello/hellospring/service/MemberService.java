0ce3c53d9519910ad994346893c04ec804a5334c
ChanSol <mutongzang@gmail.com>
2022-04-03 22:57:01 +0900

    회원 관리 예제

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//단축키 ctrl+shift+t 테스트 작성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+     회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        vaildatedDuplicateMember(member);
+        memberRepository.save(member);
+        return  member.getId();
+    }
+
+    //메소드 뽑아내기 ctrl+alt+m
+    private void vaildatedDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    /*
+    전체 회원 조회
+    */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public  Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+
+}
